b30235cef597bc89a88fdec2a48b415f83bdedfd
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-10 20:17:45 -0700

    Exercise 6

--- script.js
+++ script.js
@@ -100,3 +100,25 @@ const greetUser = (name, time) => {
 }
 
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"))
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+const maxOfThree = (x, y, z) => {
+    if (x > y && x > z) {
+        return x
+    } else if (y > x && y > z) {
+        return y
+    } else (z > x && z > y) 
+        return z
+}
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
